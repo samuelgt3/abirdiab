00050afea521c67e9db4e1387b844f91d1bfed96
Sami <143815018+samuelgt3@users.noreply.github.com>
2026-08-28 11:21:50 -0700

    connected to mysql local server

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -73,9 +73,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "/my.cnf",
+        },
     }
 }
 
